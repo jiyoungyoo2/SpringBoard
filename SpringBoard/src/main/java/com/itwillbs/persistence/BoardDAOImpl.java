2d839faace4e66fd9f9bb8516af33e0f2e27ebd3
jiyoungyoo2 <zhzh0210@gmail.com>
2024-06-18 11:41:15 +0900

    게시판 글 수정하기 완료

--- SpringBoard/src/main/java/com/itwillbs/persistence/BoardDAOImpl.java
+++ SpringBoard/src/main/java/com/itwillbs/persistence/BoardDAOImpl.java
@@ -54,6 +54,13 @@ public class BoardDAOImpl implements BoardDAO {
 		
 		return sqlSession.selectOne(NAMESPACE + "getBoard", bno);
 	}
+
+	@Override
+	public void updateBoard(BoardVO vo) throws Exception {
+		logger.debug(" updateBoard(BoardVO vo) 실행 ");
+		
+		sqlSession.update(NAMESPACE+"updateBoard", vo);
+	}
 	
 	
 	
